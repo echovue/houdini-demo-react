c072dafabe0923242f6e08b25a8e0e162fcda728
Mike Mackrory <mike@echovue.com>
2022-04-03 21:19:42 -0700

    Remove tests

--- src/App.js
+++ src/App.js
@@ -1,105 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <div class='row'>
-        <div class='column'>
-          <h3 class='header'>Traditional CSS</h3>
-          <div class='block'>
+      <div className='row'>
+        <div className='column'>
+          <h3 className='header'>Traditional CSS</h3>
+          <div className='block'>
             HTML/CSS
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block locked'>
+          <div className='arrow'>&darr;</div>
+          <div className='block locked'>
             Parsing
           </div>
-          <div class='multi-arrow-downline'>|</div>
+          <div className='multi-arrow-downline'>|</div>
           <hr width='50%'/>
-          <div class='multi-block'>
-            <div class='arrow multi-arrow'>&darr;</div>
-            <div class='arrow multi-arrow'>&darr;</div>
+          <div className='multi-block'>
+            <div className='arrow multi-arrow'>&darr;</div>
+            <div className='arrow multi-arrow'>&darr;</div>
           </div>
-          <div class='multi-block'>
-            <div class='block js-enabled'>
+          <div className='multi-block'>
+            <div className='block js-enabled'>
               DOM
             </div>
-            <div class='block locked half-js-enabled'>
+            <div className='block locked half-js-enabled'>
               CSS OM
             </div>
           </div>
-          <div class='multi-block'>
-            <div class='multi-arrow-downline'>|</div>
-            <div class='multi-arrow-downline'>|</div>
+          <div className='multi-block'>
+            <div className='multi-arrow-downline'>|</div>
+            <div className='multi-arrow-downline'>|</div>
           </div>
           <hr width='50%'/>
-          <div class='arrow'>&darr;</div>
-          <div class='block locked'>
+          <div className='arrow'>&darr;</div>
+          <div className='block locked'>
             Render Tree
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block locked'>
+          <div className='arrow'>&darr;</div>
+          <div className='block locked'>
             Layout
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block locked'>
+          <div className='arrow'>&darr;</div>
+          <div className='block locked'>
             Paint
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block locked'>
+          <div className='arrow'>&darr;</div>
+          <div className='block locked'>
             Composite
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block'>
+          <div className='arrow'>&darr;</div>
+          <div className='block'>
             Screen
           </div>
         </div>
-        <div class='column'>
-          <h3 class='header'>Houdini</h3>
-          <div class='block'>
+        <div className='column'>
+          <h3 className='header'>Houdini</h3>
+          <div className='block'>
             HTML/CSS
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block js-enabled'>
+          <div className='arrow'>&darr;</div>
+          <div className='block js-enabled'>
             Parsing
           </div>
-          <div class='multi-arrow-downline'>|</div>
+          <div className='multi-arrow-downline'>|</div>
           <hr width='50%'/>
-          <div class='multi-block'>
-            <div class='arrow multi-arrow'>&darr;</div>
-            <div class='arrow multi-arrow'>&darr;</div>
+          <div className='multi-block'>
+            <div className='arrow multi-arrow'>&darr;</div>
+            <div className='arrow multi-arrow'>&darr;</div>
           </div>
-          <div class='multi-block'>
-            <div class='block js-enabled'>
+          <div className='multi-block'>
+            <div className='block js-enabled'>
               DOM
             </div>
-            <div class='block js-enabled'>
+            <div className='block js-enabled'>
               Type OM
             </div>
           </div>
-          <div class='multi-block'>
-            <div class='multi-arrow-downline'>|</div>
-            <div class='multi-arrow-downline'>|</div>
+          <div className='multi-block'>
+            <div className='multi-arrow-downline'>|</div>
+            <div className='multi-arrow-downline'>|</div>
           </div>
           <hr width='50%'/>
-          <div class='arrow'>&darr;</div>
-          <div class='block js-enabled'>
+          <div className='arrow'>&darr;</div>
+          <div className='block js-enabled'>
             Render Tree
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block js-enabled'>
+          <div className='arrow'>&darr;</div>
+          <div className='block js-enabled'>
             Layout
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block js-enabled'>
+          <div className='arrow'>&darr;</div>
+          <div className='block js-enabled'>
             Paint
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block js-enabled'>
+          <div className='arrow'>&darr;</div>
+          <div className='block js-enabled'>
             Composite
           </div>
-          <div class='arrow'>&darr;</div>
-          <div class='block'>
+          <div className='arrow'>&darr;</div>
+          <div className='block'>
             Screen
           </div>
         </div>
